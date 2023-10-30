4551da90a516ae519b230934582e096dc2b4c549
Eray Ercan <erayercan1533@gmail.com>
2023-10-30 14:42:32 +0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,41 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+import React, {useState} from 'react'
 import music_data from './data/music-data.json'
 import MusicCard from './components/MusicCard'
+import SearchBar from './components/SearchBar'
 
 export default function App() {
+  const [musicList,setMusiclist] = useState(music_data)
 
   const renderSong = ({ item }) =>
-  <MusicCard
-    imageUrl={item.imageUrl}
-    title={item.title}
-    artist={item.artist}
-    year={item.year}
-    isSoldOut={item.isSoldOut}
-  />
+    <MusicCard
+      imageUrl={item.imageUrl}
+      title={item.title}
+      artist={item.artist}
+      year={item.year}
+      isSoldOut={item.isSoldOut}
+    />
+
   const renderSeparator = () => <View style={styles.separator} />
 
+  const handleSeach = (text) => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase()
+      const currentTitle = song.title.toLowerCase()
+
+      return currentTitle.indexOf(searchedText) > -1
+    })
+
+    setMusiclist(filteredList)
+  }
   return (
     <View>
+      <SearchBar
+        onSearch={handleSeach}
+      />
       <FlatList
         keyExtractor={item => item.id}
-        data={music_data}
+        data={musicList}
         renderItem={renderSong}
         ItemSeparatorComponent={renderSeparator}
       />
